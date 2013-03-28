919a710e842623d3d79593d25955fa8ece52bfa6
Adam Batkin <adam@batkin.net>
2013-03-28 00:44:46 -0400

    updates

--- oh-my-zsh/20-aliases.zsh
+++ oh-my-zsh/20-aliases.zsh
@@ -2,3 +2,5 @@ alias ant='ant -logger org.apache.tools.ant.listener.AnsiColorLogger'
 alias ll='ls --color=tty -lrt'
 alias kde=". ~/kde/env.sh && cd ~/kde/src"
 alias vi=vim
+alias ccp="xclip -selection c"
+
