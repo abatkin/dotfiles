d066f4cf96ffd539a8d3605a4fbcabd7a88e5778
Adam Batkin <adam@batkin.net>
2013-07-04 22:36:37 -0400

    try vi mode

--- oh-my-zsh/06-keys.zsh
+++ oh-my-zsh/06-keys.zsh
@@ -1,2 +1,15 @@
 bindkey '^[[B' down-line-or-history
 bindkey '^[[A' up-line-or-history
+
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+
+bindkey -M viins '\e[H' beginning-of-line
+bindkey -M viins '\e[F' end-of-line
+bindkey -M viins '\e[3~' delete-char
+
+bindkey -M vicmd '\e[H' beginning-of-line
+bindkey -M vicmd '\e[F' end-of-line
+bindkey -M vicmd '\e[3~' delete-char
+
+
