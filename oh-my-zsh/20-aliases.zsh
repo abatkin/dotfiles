6e078712a8c37363e096822031ff1184d8f195f6
Adam Batkin <adam@batkin.net>
2014-12-11 23:07:00 -0500

    alias grep and unset GREP_OPTIONS

--- oh-my-zsh/20-aliases.zsh
+++ oh-my-zsh/20-aliases.zsh
@@ -4,3 +4,8 @@ alias kde=". ~/kde/env.sh && cd ~/kde/src"
 alias vi=vim
 alias ccp="xclip -selection c"
 
+if [[ ! -z "$GREP_OPTIONS" ]]; then
+  alias grep="grep $GREP_OPTIONS"
+  export GREP_OPTIONS=
+fi
+
