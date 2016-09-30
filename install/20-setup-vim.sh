c1d39383de842e0c9b3355bafca748cef276bd4f
Adam Batkin <adam@batkin.net>
2016-09-30 01:16:17 -0400

    allow upstream vim module to change remote, alias name

--- install/20-setup-vim.sh
+++ install/20-setup-vim.sh
@@ -19,8 +19,12 @@ if [[ "$LOCAL_ONLY" == "true" ]]; then
   exit 0
 fi
 
-for BUNDLE_URL in $(cat $DOTFILES_ROOT/vim/bundles.txt); do
-  BUNDLE_NAME=$(echo "$BUNDLE_URL" | sed -e 's/.*\///' -e 's/\.git$//')
+while IFS="\n" read -r BUNDLE_LINE || [[ -n "$BUNDLE_LINE" ]]; do
+  echo "$BUNDLE_LINE" | IFS=" " read -r BUNDLE_URL BUNDLE_NAME
+  if [[ -z $BUNDLE_NAME ]]; then
+    BUNDLE_NAME=$(echo "$BUNDLE_URL" | sed -e 's/.*\///' -e 's/\.git$//')
+  fi
+
   SKIP_MODULE=0
   for EXCLUDE in $(echo $BLACKLIST_VIM_MODULES); do
     if [[ "$EXCLUDE" == "$BUNDLE_NAME" ]]; then
@@ -34,12 +38,24 @@ for BUNDLE_URL in $(cat $DOTFILES_ROOT/vim/bundles.txt); do
       git clone "$BUNDLE_URL" $BUNDLE_DIR/$BUNDLE_NAME
     else
       pushd $BUNDLE_DIR/$BUNDLE_NAME
-      git pull
+      echo "Updating $BUNDLE_NAME ($BUNDLE_URL)"
+      CURRENT_BRANCH=$(git symbolic-ref -q --short HEAD) > /dev/null 2>&1
+      CURRENT_REMOTE=$(git config branch.${CURRENT_BRANCH}.remote) > /dev/null 2>&1
+      CURRENT_REMOTE_URL=$(git config remote.${CURRENT_REMOTE}.url) > /dev/null 2>&1
+      if [[ -z $CURRENT_REMOTE ]]; then
+        echo "Unable to determine branch/remote for $BUNDLE_NAME"
+      else
+        if [[ $CURRENT_REMOTE_URL == $BUNDLE_URL ]]; then
+          git pull
+        else
+          echo "Warning: Bundle $BUNDLE_NAME appears to have switched remotes ($CURRENT_REMOTE => $BUNDLE_URL)"
+        fi
+      fi
       popd
     fi
   else
     echo "Skipping locally blacklisted Vim module $BUNDLE_NAME"
   fi
-done
+done < $DOTFILES_ROOT/vim/bundles.txt
 
 
