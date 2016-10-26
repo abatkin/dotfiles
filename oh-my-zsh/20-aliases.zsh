fb240122ed072a7e0be00db1880e0317100d069d
Adam Batkin <adam@batkin.net>
2016-10-26 00:41:23 -0400

    fix ls quoting due to irresponsible bash maintainers

--- oh-my-zsh/20-aliases.zsh
+++ oh-my-zsh/20-aliases.zsh
@@ -1,5 +1,6 @@
 alias ant='ant -logger org.apache.tools.ant.listener.AnsiColorLogger'
-alias ll='ls --color=tty -lrt'
+alias ll='ls --color=tty -lrtN'
+alias ls='ls --color=tty -N'
 alias kde=". ~/kde/env.sh && cd ~/kde/src"
 alias vi=vim
 alias ccp="xclip -selection c"
