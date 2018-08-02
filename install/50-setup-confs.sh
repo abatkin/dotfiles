a1462d72e1247643aaf1da8413ad2eae68eefc48
Adam Batkin <adam@batkin.net>
2018-08-01 20:42:17 -0400

    fix install script

--- install/50-setup-confs.sh
+++ install/50-setup-confs.sh
@@ -3,6 +3,7 @@
 CONFS_DIR=$DOTFILES_ROOT/confs
 INSTALL_DOTFILE=$DOTFILES_SCRIPTS/install-dotfile.sh
 
+mkdir -p $HOME/.config/kitty
 $INSTALL_DOTFILE $CONFS_DIR kitty.conf .config/kitty/kitty.conf
 
 
