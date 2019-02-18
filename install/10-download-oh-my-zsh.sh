a747ff7782f1a83d2443ce8501d4a44f7de20820
Adam Batkin <adam@batkin.net>
2019-02-17 22:37:36 -0500

    switch from git:// to https:// urls

--- install/10-download-oh-my-zsh.sh
+++ install/10-download-oh-my-zsh.sh
@@ -3,7 +3,7 @@
 echo "Checking out oh-my-zsh"
 if [[ ! "$LOCAL_ONLY" == "true" ]]; then
   if [[ ! -d ~/.oh-my-zsh ]]; then
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
   else
     pushd ~/.oh-my-zsh
     git pull
