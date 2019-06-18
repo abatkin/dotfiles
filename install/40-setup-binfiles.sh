41cd2e3f5e227637426dc19c17acfbae1b925d15
Adam Batkin <adam@batkin.net>
2019-06-17 21:33:49 -0400

    create ~/bin if necessary

--- install/40-setup-binfiles.sh
+++ install/40-setup-binfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p ~/bin
+
 for FILE in $DOTFILES_ROOT/binfiles/*; do
   BASENAME=$(basename $FILE)
   $DOTFILES_SCRIPTS/install-dotfile.sh $DOTFILES_ROOT/binfiles $BASENAME bin/$BASENAME
