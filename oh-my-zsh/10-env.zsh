44b07023bd2c144fced3b2e1957a9876ad32a080
Adam Batkin <adam@batkin.net>
2022-09-13 23:48:06 -0400

    add ~/bin to path?

--- oh-my-zsh/10-env.zsh
+++ oh-my-zsh/10-env.zsh
@@ -6,4 +6,5 @@ export PAGER=less
 [[ -n $MAVEN_HOME && -e $MAVEN_HOME ]] && export PATH="$MAVEN_HOME/bin:$PATH"
 [[ -n $ANT_HOME && -e $ANT_HOME ]] && export PATH="$ANT_HOME/bin:$PATH"
 [[ -n $JAVA_HOME && -e $JAVA_HOME ]] && export PATH="$JAVA_HOME/bin:$PATH"
+[[ -e $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
 
