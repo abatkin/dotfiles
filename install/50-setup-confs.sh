95b8a06c43fec5f9713ec390d70ec58d8ec46ac5
Adam Batkin <adam@batkin.net>
2023-01-13 00:02:13 -0500

    add starship support

--- install/50-setup-confs.sh
+++ install/50-setup-confs.sh
@@ -5,6 +5,7 @@ INSTALL_DOTFILE=$DOTFILES_SCRIPTS/install-dotfile.sh
 
 mkdir -p $HOME/.config/kitty
 $INSTALL_DOTFILE $CONFS_DIR kitty.conf .config/kitty/kitty.conf
+$INSTALL_DOTFILE $CONFS_DIR starship.toml .config/starship.toml
 
 if [[ ! -e $HOME/.config/launch-entries.ini ]]; then
   $INSTALL_DOTFILE $CONFS_DIR launch-entries.ini .config/launch-entries.ini
