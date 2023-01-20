cc5f73f512d91fc373a3438fb7238a048a9642bf
Adam Batkin <adam@batkin.net>
2023-01-19 21:24:00 -0500

    add rofi configs

--- install/50-setup-confs.sh
+++ install/50-setup-confs.sh
@@ -6,6 +6,8 @@ INSTALL_DOTFILE=$DOTFILES_SCRIPTS/install-dotfile.sh
 mkdir -p $HOME/.config/kitty
 $INSTALL_DOTFILE $CONFS_DIR kitty.conf .config/kitty/kitty.conf
 $INSTALL_DOTFILE $CONFS_DIR starship.toml .config/starship.toml
+mkdir -p $HOME/.config/rofi
+$INSTALL_DOTFILE $CONFS_DIR config.rofi .config/rofi/config.rofi
 
 if [[ ! -e $HOME/.config/launch-entries.ini ]]; then
   echo "Copying template for .config/launch-entries.ini"
