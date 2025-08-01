64ec856928cb0f57c272d2d682829772d7e49571
Adam Batkin <adam@batkin.net>
2025-08-01 00:03:58 -0400

    try out mise

--- install/50-setup-confs.sh
+++ install/50-setup-confs.sh
@@ -8,6 +8,8 @@ $INSTALL_DOTFILE $CONFS_DIR kitty.conf .config/kitty/kitty.conf
 $INSTALL_DOTFILE $CONFS_DIR starship.toml .config/starship.toml
 mkdir -p $HOME/.config/rofi
 $INSTALL_DOTFILE $CONFS_DIR config.rasi .config/rofi/config.rasi
+mkdir -p $HOME/.config/mise
+$INSTALL_DOTFILE $CONFS_DIR mise-config.toml .config/mise/config.toml
 
 $INSTALL_DOTFILE $DOTFILES_ROOT nvim .config/nvim
 
