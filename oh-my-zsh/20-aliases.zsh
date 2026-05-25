7091fc09bdbef0f8caade2cd57a5e9b675887988
Adam Batkin <adam@batkin.net>
2026-05-25 00:16:20 -0400

    small fixes

--- oh-my-zsh/20-aliases.zsh
+++ oh-my-zsh/20-aliases.zsh
@@ -8,14 +8,14 @@ if [[ ! -z "$GREP_OPTIONS" ]]; then
   export GREP_OPTIONS=
 fi
 
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urldecode='python3 -c "import sys, urllib.parse; print(urllib.parse.unquote_plus(sys.argv[1]))"'
 
 
 if [[ -e /usr/share/skim/shell/key-bindings.zsh && -z $DISABLE_SKIM_KEYBINDINGS ]]; then
   . /usr/share/skim/shell/key-bindings.zsh
 fi
 
-alias xl='exa --group-directories-first --classify --git'
+alias xl='eza --group-directories-first --classify --git'
 alias xll='xl -l -s modified'
 
 alias skvi='f(){ x="$(sk --bind "ctrl-p:toggle-preview" --ansi --preview="bat {} --color=always" --preview-window=right:60%:hidden)"; [[ $? -eq 0 ]] && vim "$x" || true }; f'
